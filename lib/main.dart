0ccbb3937885428d077db0306fc2eef17a2c3c90
kevinmitchell36 <kevingm31@gmail.com>
2024-04-22 15:02:48 -0500

    Add custom class
    Learning about custom classes that allow for separated code.

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,32 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 83, 24, 185),
-              Color.fromARGB(255, 148, 106, 221)
-            ]),
-          ),
-          child: const Center(
-            child: Text('Hello World!'),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 83, 24, 185),
+            Color.fromARGB(255, 148, 106, 221)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
