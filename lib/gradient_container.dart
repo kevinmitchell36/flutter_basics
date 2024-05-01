c8f3339d26d24e52102092cdb0a69a56f085c864
kevinmitchell36 <kevingm31@gmail.com>
2024-05-01 12:00:23 -0500

    Update classes to accept arguments
    Learn how to pass arguments into class.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:basics/styled_text.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
-            Color.fromARGB(255, 83, 24, 185),
-            Color.fromARGB(255, 148, 106, 221)
-          ],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText()),
+      child: const Center(child: StyledText('Hello World!')),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,11 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(Color.fromARGB(255, 83, 24, 185),
+         Color.fromARGB(255, 148, 106, 221)
+        ),
       ),
     ),
   );
 }
+
